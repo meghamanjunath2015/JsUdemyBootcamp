48011adfd348832800d2e50796939b046d6f49c8
meghamanjunath2015 <meghamanjunath2015@gmail.com>
2023-04-17 17:10:26 -0700

    complete arrays

--- Arrays/filter.js
+++ Arrays/filter.js
@@ -16,7 +16,7 @@ const noteObj = [{
 //filter the notes with 'ne' in it 
 const findNote = function(notes, query){
     const searchText = query.toLowerCase()
-    return noteObj.filter(function(note, index){
+    return notes.filter(function(note){
     return (note.title.toLowerCase().includes(searchText) ||note.body.toLowerCase().includes(searchText))
     })
 }
@@ -24,3 +24,39 @@ const findNote = function(notes, query){
 console.log(findNote(noteObj, 'ne'))
 
 //filter yet to be completed list 
+const todoObjList = [{
+    text: 'Order dog food',
+    completed: true
+},
+{
+    text: 'Order cat food',
+    completed: false
+}, 
+{
+    text: 'Buy food',
+    completed: false
+}, 
+{
+    text: 'Excercise',
+    completed: false
+}, 
+{
+    text: 'Garage re-paint',
+    completed: true
+}, 
+{
+    text: 'Fix backdoor',
+    completed: true
+},
+{
+    text: 'Water plants',
+    completed: false
+}]
+
+const filterRemainingToDo =function(todoList){
+    return todoList.filter(function(todoList){
+        return todoList.completed == false
+    })
+
+}
+console.log(filterRemainingToDo(todoObjList))
