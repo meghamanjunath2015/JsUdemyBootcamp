c985a28b1ed089b88be0643dab7c623490d1427a
meghamanjunath2015 <meghamanjunath2015@gmail.com>
2023-04-19 09:51:19 -0700

    add and delete files

---
+++ jsInBrowser/notesApp/notes-app.js
@@ -0,0 +1,16 @@
+/*
+//query and remove first occurance 
+
+const p = document.querySelector('p')
+console.log(p)
+//p.remove()
+
+//Query all and remove, get text and set text
+
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p){
+    //p.textContent = '###'
+    //console.log(p.textContent)
+    //p.remove()
+})
+*/
