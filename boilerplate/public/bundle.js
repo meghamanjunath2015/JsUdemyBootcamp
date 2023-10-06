6334bd0cc6c571377a28a373e6c6d2f42b07b1fc
meghamanjunath2015 <meghamanjunath2015@gmail.com>
2023-10-06 11:43:33 -0700

    add webpack

--- boilerplate/public/bundle.js
+++ boilerplate/public/bundle.js
@@ -4,7 +4,8 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var nameP = 'Mike';
+var nameP = 'Maggie';
+console.log('Hello')
 console.log(nameP);
 
 var Hangman = function () {
