8524606523a5e916935905ec89687c1323811416
meghamanjunath2015 <meghamanjunath2015@gmail.com>
2024-02-28 16:32:48 -0800

    convert notes function to webpack app

---
+++ NotesApp/source/notes-edit.js
@@ -0,0 +1,29 @@
+'use strict'
+import {updateNote, generateLastEdited, stripId, getSavedNotes} from './notes-functions'
+
+//get the note
+const getNoteid = stripId()
+console.log(getNoteid)
+const noteList = getSavedNotes()
+const note = noteList.find((note) => note.id === getNoteid)
+
+//show the existing body and title
+let getPreviousBody = note.body
+let getPreviousTitle = note.title
+let getLastEdited = document.querySelector('#last-edited')
+
+document.querySelector('#edit-title').value = getPreviousTitle
+//convert it to date in moment()
+getLastEdited.textContent = generateLastEdited(note.updatedAt)
+document.querySelector('#edit-body').value = getPreviousBody
+
+
+//Add new note
+document.querySelector('#notes-edit-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    getPreviousTitle = e.target.editTitle.value
+    getPreviousBody = e.target.editBody.value
+    updateNote(note, getPreviousTitle, getPreviousBody)
+    //redirection 
+    location.assign('index.html')
+})
